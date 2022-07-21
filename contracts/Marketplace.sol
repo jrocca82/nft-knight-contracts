aac613f3af1b8d944b17a10740b7703083ec872e
Jo <loluvulol@gmail.com>
2022-07-21 13:19:45 +1000

    Update

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,26 +3,27 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 import "./GameItems.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Marketplace {
     GameItems private _item;
+    ERC20 private _potato;
 
     //Map item ID to price
     mapping(uint => uint) price;
 
-    constructor(GameItems item) {
+    constructor(GameItems item, ERC20 potato) {
         require(address(item) != address(0));
         _item = item;
+        _potato = potato;
         price[0] = 100000000000000;
-        price[1] = 200000000000000;
+        price[1] = 100000000000000;
         price[2] = 300000000000000;
     }
 
     function buyItem(uint _itemId) public payable {
-        uint sentAmount = msg.value;
-        require(sentAmount >= price[_itemId], "Not enough funds");
         require(price[_itemId] != 0, "This item is not available");
-
+        _potato.transferFrom(msg.sender, address(this), price[_itemId]);
         _item.mint(_itemId, 1, msg.sender);
     }
 }
